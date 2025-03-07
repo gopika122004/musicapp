4b610cf9e1ac3653b948747413c915f8ba0d1641
Gopikasuresh <145031839+gopika122004@users.noreply.github.com>
2025-03-07 13:43:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '', // Set a default base path
+  base: '/Rythamictunes/', // Set a default base path
   plugins: [react()],
   
 })
